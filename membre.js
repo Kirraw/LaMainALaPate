819ee096f0d20292329ecb4a92e31eaabd8203e0
Aurélie Praira~ <aureliepa1010@gmail.com>
2023-10-06 08:49:13 +0200

    ajout rectangle

--- membre.js
+++ membre.js
@@ -1,4 +1,16 @@
+function canvasFun(){
+    var canvas = document.getElementById('canvas1');
+if (canvas.getContext)
+{var ctx = canvas.getContext('2d');
+ctx.font = "12pt skullbones,Arial";
+ctx.strokeStyle= "rgb(0,0,0)";
+ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.fillStyle = "rgb(0,20,180)";
+    ctx.strokeText("Exemple de texte", 10, 20);
+    ctx.fillText("Autre exemple", 10, 60);
 
-
+}
+}
+window.onload=canvasFun;
 
 
