634670291ff1d00e7f03843fe50604fb5ed71eae
Aurélie Praira~ <aureliepa1010@gmail.com>
2023-10-12 09:11:24 +0200

    modif membre js

--- js/membre.js
+++ js/membre.js
@@ -59,59 +59,6 @@ bouton.addEventListener("click", function() {
 
 
 
-// // ajouter un membre dans un formulaire
-
-// let nom = inputNom.value;
-// let inputNom = document.getElementById("userName");
-
-// let inputPin = document.getElementById("password");
-// let pin = inputPin.value;
-
-
-// // choix de l'avatar
-// let avatar = "";
-// for (let i=1; i<=12; i++) {
-//     const element = document.getElementById("avatar"+i);
-//     if (element.checked) {
-//         avatar = element.value;
-//         break;
-//     } 
-// }
-
-// //choix des couleurs
-// let couleur = "";
-// for (let i=1; i<=6; i++) {
-//     const element = document.getElementById("color"+i);
-//     if (element.checked) {
-//         couleur = element.value;
-//         break;
-//     }
-// }
-
-// // variable formulaire 
-
-// class formerProfile = {
-//     let nom = inputNom.value;
-//     let pin = inputPin.value;
-//     let avatar = "";
-//     let couleur = "";
-// }
-
-// //variable validation du nouveau membre
-
-// let value = true;
-
-
-// //affichage du rectangle
-
-
-
-
-// //function Users(id, type, nom, pin, avatar, couleur) 
-//     //{ this.id = id;
-//         // this.type = type;
-//         // this.nom = nom;
-//         // this.pin = pin;
-//         // this.avatar = avatar;
-//         // this.couleur = couleur;
+var canvas = document.getElementById('canvas1');
+var contexte = canvas.getContent('2d');
 
